059ead9de33c6fe38bda0945befd867851f748f6
susanna.stepanian <sanastepanyan@gmail.com>
2024-07-09 22:51:21 +0300

    day 17

---
+++ src/main/java/org/example/day17/Kata17.java
@@ -0,0 +1,10 @@
+package org.example.day17;
+
+public class Kata17 {
+
+    public static String maskify(String str) {
+        if (str == null || str.length() < 5) return str;
+        return "#".repeat(str.length() - 4) + str.substring(str.length() - 4);
+    }
+
+}
---
+++ src/main/java/org/example/day17/Leet17.java
@@ -0,0 +1,34 @@
+package org.example.day17;
+
+class Leet17 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val){
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+        return result.next;
+    }
+
+}
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
